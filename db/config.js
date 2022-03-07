e02aafc602b628a64450466074b92369906103cc
Carlos Andres Reyes Peña <carlosarp2000@gmail.com>
2022-03-07 17:24:23 -0500

    some preparations for migrations

--- db/config.js
+++ db/config.js
@@ -1,19 +1,17 @@
-const { Sequelize } = require('sequelize');
-
 const {config} = require('../config/config')
-const setupModels = require('../db/models')
 
 const USER = encodeURIComponent(config.dbUser)
 const PASSWORD = encodeURIComponent(config.dbPassword)
 const URI = `mysql://${USER}:${PASSWORD}@${config.dbHost}:${config.dbPort}/${config.dbName}`
 
-const sequelize = new Sequelize(URI,
-  {dialect: 'mysql',
-  loging: true,
-});
+module.exports = {
+  development: {
+    url: URI,
+    dialect: 'mysql',
+  },
 
-setupModels(sequelize)
-
-sequelize.sync();
-
-module.exports = sequelize;
+  production: {
+    url: URI,
+    dialect: 'mysql',
+  }
+}
